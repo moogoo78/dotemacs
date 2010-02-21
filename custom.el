2ab1f88d33f60dbd7bcbd30366ed1fffc538276b
moogoo <moogoo@monki.(none)>
2010-02-21 16:08:09 +0800

    agenda file add journal

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
   ;; If there is more than one, they won't work right.
  '(delete-selection-mode nil)
  '(mark-even-if-inactive t)
+ '(org-agenda-files (quote ("~/orgs/my/journal.org" "/home/moogoo/orgs/my/marathon.org" "/home/moogoo/orgs/my/my.org" "/home/moogoo/orgs/my/private.org" "/home/moogoo/orgs/my/project.org" "/home/moogoo/orgs/my/read.org" "/home/moogoo/orgs/my/todo.org" "/home/moogoo/orgs/my/work.org")))
  '(org-startup-folded (quote content))
  '(scroll-bar-mode (quote right))
  '(transient-mark-mode 1))
