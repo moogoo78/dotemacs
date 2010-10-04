d821b91822d4de3fc0a02471d0bd4f572f0d3e7a
moogoo <moogoo@rambo.(none)>
2010-10-04 23:55:05 +0800

    大整理, init.el

--- init.el
+++ init.el
@@ -1,221 +1,25 @@
-﻿; ====== MooGoo's .emacs ======
-;;;; general setting
-;; set default path
-(setq my-path "~/.emacs.d/"
-      my-path-elisp (concat my-path "moogoo-elisp/")
-      my-path-plugin (concat my-path "goodies/")
-      default-directory "~/")
+﻿; === MooGoo's .emacs ===
+; == set path ===
+
 (if (eq system-type 'gnu/linux)
     (defvar mg-org-file "~/Dropbox/org"))
 (if (eq system-type 'windows-nt)
     (defvar mg-org-file "C:/Documents and Settings/moogoo/My Documents/My Dropbox/org"))
 
+(defvar my-path "~/.emacs.d/")
 (add-to-list 'load-path my-path)
-(add-to-list 'load-path my-path-plugin)
-(add-to-list 'load-path my-path-elisp)
+(add-to-list 'load-path (concat my-path "goodies"))
 
-;; frame title
-(setq frame-title-format "%b - %f"   ; frame title, http://www.emacswiki.orgemacs/FrameTitle
-      icon-title-format "Emacs - %b")
+(load "init-mg-generic.el")
+(load "init-mg-keybinding.el")
+(load "init-mg-calendar.el")
 
-;; environment
-(setq inhibit-startup-message   t   ; no startup message
-      make-backup-files         nil ; no backup files
-      auto-save-list-file-name  nil ; no .saves files
-      auto-save-default         nil ; no auto savingan
-      case-fold-search          t   ; search ignore case
-      iswitchb-mode             1   ; better than switch-to-buffer (C-x b)
-      kill-ring-max             200 ;
-      visible-bell              t   ; disable beep in linux (openSUSE)
-      ; scroll bar
-      ;scroll-bar-mode        'right ;
-      scroll-margin 3 
-      scroll-conservatively 10000  ; margin 3 line when up or down
-      x-select-enable-clipboard t
-      interprogram-paste-function 'x-cut-buffer-or-selection-value)
-
-
-(blink-cursor-mode nil)   ; disable cursor blink
-(tool-bar-mode         nil) ;
-(global-linum-mode     t) ; line number
-(column-number-mode    t) ; show column-number
-;(ido-mode              t) ; enable ido-mode
-(cua-mode              t) ; 
-(windmove-default-keybindings)      ; shift + 4way key jump window
-(setq default-major-mode 'text-mode)     ; default text-mode
-;(server-start)            ; start emacs server daemon
-;; highlight
-(setq search-highlight           t  ; highlight search
-      query-replace-highlight    t  ; highlight query object
-      mouse-sel-retain-highlight t)  ; Keep mouse high-lightening ??
-(global-hl-line-mode 1)         ; highlight current line
-(setq default-cursor-type 'hbar)    ; make cursor underline bar
-
-;; unicode
-(prefer-coding-system       'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-
-;; indent
-(setq standard-indent   2  ; set indent size
-      default-tab-width 2) ;
-(setq-default indent-tabs-mode nil) ;; indent to space
-;(setq-default indent-tabs-mode nil)
-;(setq tab-stop-list ())
-
-
-;;;; key-binding
-(global-set-key "\C-l" 'goto-line)            ; go to line num
-(global-set-key [f5] 'compile)                ; make
-;(global-set-key [f10] 'kill-buffer)
-(global-set-key [f11] 'org-agenda)
-(global-set-key [f12] 'calendar)
-
-;(global-set-key [f1] 'dired-single-magic-buffer)
-(global-set-key [f1] 'delete-other-windows)
-(global-set-key [f2] 'split-window)
-(global-set-key [f3] 'split-window-horizontally)
-;(global-set-key [f4] 'kill-buffer)
-(global-set-key [f6] 'switch-to-buffer)
-(global-set-key [f8] 'org-export-as-html)
-;(global-set-key '[(control c) (d)] 'delblank)
-(global-set-key (kbd "C-0") 'kill-buffer) 
-(global-set-key (kbd "C-3") 'set-mark-command) ; from: http://jidanni.org/comp/configuration/.emacs
-(global-set-key (kbd "C-2") 'yy) ; copy one line and paste
-
-;; macro
-(fset 'yy
-   [?\C-a ?\C-@ ?\C-e ?\M-w return ?\C-y ?\C-a])
-(fset 'yyc
-   [?\C-a ?\C-@ ?\C-e ?\M-w return ?\C-y up ?\C-a ?/ ?/])
-(fset 'cc
-   "\C-a//")
-(fset 'delblank
-   "\260\C-k")
-
-;;;; style
-;; fonts
-;; I keep all my emacs-related stuff under ~/emacs
-(if (eq system-type 'gnu/linux)
-    (set-default-font "-unknown-Droid Sans Mono-normal-normal-normal-*-13-*-*-*-*-0-iso10646-1"))
-;(set-default-font " -unknown-Droid Sans Mono-normal-normal-normal-*-13-*-*-*-*-0-iso10646-1")
-;(set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-15-*-*-*    -m-0-iso10646-1")
-
-(load-theme 'mg)
-
-(setq diary-file (concat my-path ".diary"))
-
-;;;; bookmark
-(setq bookmark-default-file (concat my-path ".bookmarks"))
-(setq bookmark-save-flag 1)
-
-
-;;;; org-mode
-(setq org-todo-keywords '("TODO" "STARTED" "WAITING" "DONE"))
-(setq org-agenda-include-all-todo t)
-(setq org-agenda-include-diary t)
-;(require 'org-babel-init)  
-(setq org-directory mg-org-file)
-;(set org-agenda files (file-expand-wildcards "~/org/*.org"))
-;(setq org-agenda-files '(concat mg-org-file "/my/project.org")) why ....
-(if (eq system-type 'gnu/linux)
-    (setq org-agenda-files '(
-"~/Dropbox/org/my/project.org" 
-"~/Dropbox/org/my/schedule.org"
-)))
-(if (eq system-type 'windows-nt)
-    (setq org-agenda-files '(
-"C:/Documents and Settings/moogoo/My Documents/My Dropbox/org/my/project.org" 
-"C:/Documents and Settings/moogoo/My Documents/My Dropbox/org/my/schedule.org"
-)))
-
-
-(setq org-agenda-custom-commands 
-      '(("c" "Desk Work" tags-todo "computer" ;; (1) (2) (3) (4)
-         ((org-agenda-files '("c:/schedule.org")) ;; (5)
-          (org-agenda-sorting-strategy '(priority-up effort-down))) ;; (5) cont.
-         ("~/computer.html")) ;; (6)
-        ;; ...other commands here
-        ))
-
-;; org-remember
-(org-remember-insinuate)
-;(setq org-default-notes-file /notes.org"))
-(define-key global-map "\C-cr" 'org-remember)
-(setq org-remember-templates
-      '(("Journal" ?j "* %^t %^{title} %i%^G\n  %i1. %^{social}\n  2. %^{work}\n  3. %^{life}\n  4. %^{love}" "~/orgs/my/journal.org" "Journal")
-        ("Todo" ?t "* TODO %?\n  %i\n  %a" "~/orgs/my/todo.org" "Tasks")
-        ("Quote" ?q "* %?" "~/orgs/my/quote.org" "Quote")
-        ("Read" ?r "* %?" "~/orgs/my/read.org" "Read")
-        ("Marathon" ?m "* %t %^{公里}km, %^{時間}m\n  %i%?\n" "~/orgs/my/marathon.org" "Marathon")))
-;              social
-;           2.   ^ 1.
-;           work | social/family
-;                |
-; rational ----------> emotional
-;        3. life | 4. love
-;   (money/sport)|
-;             personal
-;
-; TODO, 可對照去年同一天?
-
-;
-
- ;    (setq org-remember-templates
-  ;         '(("k" ?t "* 苦  %? %U %i\n\n  %a" "~/note.org" "無明")
-   ;     ("Journal" ?j "* %U %?\n\n  %i\n  %a" "~/org/JOURNAL.org")
-    ;    ("Idea" ?i "* %^{Title}\n  %i\n  %a" "~/org/JOURNAL.org" "New Ideas")))
-
-;; org-publish
-(require 'org-publish) 
-(setq org-publish-project-alist
-      '(
-        ("org-notes"
-         :base-directory "~/Dropbox/org"
-         :base-extension "org"
-         :publishing-directory "~/org-pub"
-         :exclude "priv-*"
-         :recursive t
-         :publishing-function org-publish-org-to-html
-         :headline-levels 4             ; Just the default for this project.
-         :language "zh"
-         ;:htmlized-source t
-;         :auto-preamble t
-         :style "<link rel='stylesheet' type='text/css' href='css/style.css' />"
-         :style-include-default nil
-         :author "MooGoo"
-         :email nil
-         ;:org-publish-use-timestamps-flag nil
-         ;:auto-index t                  ; Generate index.org automagically...
-         ;:makeindex t
-         ;:index-filename "sitemap.org"  ; ... call it sitemap.org ...
-         ;:index-title "Sitemap"         ; ... with title 'Sitemap'.
-         )
-	      ;; These are static files (images, pdf, etc)
-	      ("org-static"
-	        :base-directory "~/Dropbox/org/" ;; Change this to your local dir
-          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|txt\\|asc"
-          :publishing-directory "~/org-pub"
-	        :recursive t
-          :publishing-function org-publish-attachment
-        )
-	      ("orgpub" :components ("org-notes" "org-static"))
-        )
-)
-
-(load-file (concat my-path "mg-calendar.el"))
-(load-file (concat my-path "mg-plugin.el"))
-;; 用autoload就可以了吧~
-(mg-plugin-php)
-(mg-plugin-dot)
-;(mg-plugin-w3m)
-;(mg-plugin-single)
-;(mg-plugin-tabbar)
-;(mg-plugin-ecb)\
-;(mg-plugin-csound)
-;(mg-plugin-emms)
-
+; php
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+; graphviz
+(autoload 'graphviz-dot-mode "graphviz-dot-mode" "Major mode for editing graphviz dot." t)
+(add-to-list 'auto-mode-alist '("\\.dot\\'" . graphviz-dot-mode))
 ;; TODO
 ;(when (require 'actionscript-mode nil t)
 ;  (setq auto-mode-alist
@@ -223,33 +27,11 @@
 (autoload 'actionscript-mode "actionscript-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.as\\'" . actionscript-mode))
 
-;;;; ENHANCEMENT!
-;; like vim's yy, p
-(defadvice kill-ring-save (before slick-copy activate compile)
-  "When called interactively with no active region, copy a single line instead."
-  (interactive
-   (if mark-active (list (region-beginning) (region-end))
-     (message "Copied line")
-         (list (line-beginning-position)
-               (line-beginning-position 2)))))
-(defadvice kill-region (before slick-cut activate compile)
-  "When called interactively with no active region, kill a single line instead."
-  (interactive
-   (if mark-active (list (region-beginning) (region-end))
-     (list (line-beginning-position)
-           (line-beginning-position 2)))))
-;; via: http://www.emacswiki.org/emacs/SlickCopy
-
-
-;;;; custom
-(setq custom-file (concat my-path "custom.el"))
-(load custom-file)
-
 ;;;; sandbox
 ;; csd-mode TODO!!!
 ;(require 'csd-mode)
-(autoload 'csd-mode "csd-mode" "Major mode for editing php code." t)
-(add-to-list 'auto-mode-alist '("\\.csd\\'" . csd-mode))
+;(autoload 'csd-mode "csd-mode" "Major mode for editing php code." t)
+;(add-to-list 'auto-mode-alist '("\\.csd\\'" . csd-mode))
 
 ;;;;  Future
 ;; show time on title, but I want to show chinese cal!!
@@ -290,9 +72,9 @@
 
 ;(global-set-key [f11] 'insert-moogoo)
 ;; >>
-; gdb gui
-(setq gdb-many-windows t)
-;(setq gdb-show-main t)
+
 
 ;; ref
 ; http://sites.google.com/site/steveyegge2/my-dot-emacs-file
+; http://xahlee.org/emacs/xah_emacs_init.el
+; http://hi.baidu.com/yangyingchao/blog/item/070d047a7a3d8fe62e73b3cf.html
--- moogoo-elisp/csd-syntax.el
+++
@@ -1,9 +0,0 @@
-(defvar csd-syntax-opcodes
-  '("oscil" "ftgen" "oscil" "pluck")
-)
-(defvar csd-syntax-keyword
-  '("out" "outs" "oscil" "pluck")
-)
-(defvar csd-syntax-keyword
-  '("out" "outs" "oscil" "pluck")
-)
