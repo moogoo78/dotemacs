bbed38a814d40b98bb4401d51742fae2b3a9fb36
MooGoo <moogoo78@gmail.com>
2012-10-31 13:12:14 +0800

    coding

--- init-generic.el
+++ init-generic.el
@@ -30,9 +30,11 @@
 ;(server-start)            ; start emacs server daemon
 ; == coding ==
 (prefer-coding-system       'utf-8)
+(setq locale-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
 
 
 ; === Style ===
