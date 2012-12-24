ac662cce431b82fdf0d00c7d0c776886e11487aa
MooGoo <moogoo78@gmail.com>
2012-12-24 11:31:52 +0800

    C-x, C-t 互換

--- init-keybinding.el
+++ init-keybinding.el
@@ -39,6 +39,9 @@
 
 (global-set-key (kbd "C-b") 'kill-line)
 
+; C-x C-t互換 (fordv orak layout)
+(keyboard-translate ?\C-t ?\C-x)
+(keyboard-translate ?\C-x ?\C-t)
 
 ;; emacs 24 error
 (setq interprogram-paste-function 'x-selection-value)
@@ -197,3 +200,22 @@ on each side of cursor."
    "My default directory: " (format "%s" "~/")))
 
 ;  (setq default-directory mydir))
+
+
+
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+;(defun rename-file-and-buffer (new-name)
+(defun rename-this-file (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
