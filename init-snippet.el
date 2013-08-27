37fdfdb2eceff24cffd54957e07ff3b1467851f8
MooGoo <moogoo78@gmail.com>
2013-08-27 18:21:43 +0800

    add skeleton insert-pelican-template

--- init-snippet.el
+++ init-snippet.el
@@ -15,3 +15,15 @@
   "Insert current date"
   (interactive)
   (insert (format-time-string "%Y-%m-%d %H:%M")))
+
+
+(define-skeleton insert-pelican-template
+  "Insert templete for pelican static blog system"
+  "" (skeleton-read "Title: ")?\n
+  "#############################" ?\n
+  ":date: " (insert-current-date) ?\n
+  ":category: " (skeleton-read "category: ") ?\n
+  ":tags: " (skeleton-read "tags: ") ?\n
+  ":slug: " (skeleton-read "slug: ") ?\n
+)
+
