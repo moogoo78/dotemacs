acf96a1ec9fc8ecfa7e170b239a6c63a1c3d561c
MooGoo <moogoo78@gmail.com>
2022-01-05 06:16:32 +0800

    comment installed package by melpa

--- init.el
+++ init.el
@@ -20,6 +20,9 @@
 ;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
+; installed
+; - rjsx-mode
+
 
 (defun set-default-directory()
   "select default directory by "
