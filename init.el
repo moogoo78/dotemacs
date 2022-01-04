43ebb96af9e541f6798a687679db0b31df10538c
MooGoo <moogoo78@gmail.com>
2022-01-05 06:15:14 +0800

    add install melpa package management

--- init.el
+++ init.el
@@ -15,6 +15,11 @@
 ;(if window-system
 ;  (set-frame-size (selected-frame) 150 45))
 
+;; install melpa
+(require 'package)
+;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
 
 (defun set-default-directory()
   "select default directory by "
